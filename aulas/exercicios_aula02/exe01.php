fb6a8f610a786ffccb4f7df991c06c5e203f0811
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2025-02-25 12:44:13 -0300

    Correção dos exercícios - Aula 02

---
+++ aulas/exercicios_aula02/exe01.php
@@ -0,0 +1,11 @@
+<?php
+    $nome = "Anderson";
+    $idade = 50;
+    $cidade = "Ribeirão Pires";
+
+    echo "Seu nome é: $nome <br>";
+    echo "Sua idade é: $idade <br>";
+    echo "Sua cidade é: $cidade <br>";
+    
+
+?>
---
+++ aulas/exercicios_aula02/exe02.php
@@ -0,0 +1,12 @@
+<?php
+    $numero = -5;
+    
+    if ($numero<0){
+        echo "NEGATIVO";
+    }elseif ($numero == 0) {
+        echo "IGUAL A ZERO";
+    }else {
+        echo "POSITIVO";
+    }    
+
+?>
